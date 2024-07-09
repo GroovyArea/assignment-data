eebf7cc03227fe23b6e1bc0bfd8b212ce89788f8
GroovyArea <095201a@naver.com>
2024-07-09 21:44:09 +0900

    refactor : DDL 적용 및 JPA Auditing 적용

--- cash-note/src/main/kotlin/com/groovyarea/assignment/cashnote/common/config/AppConfig.kt
+++ cash-note/src/main/kotlin/com/groovyarea/assignment/cashnote/common/config/AppConfig.kt
@@ -1,10 +1,8 @@
-package com.groovyarea.assignment.datatransfer.common.config
+package com.groovyarea.assignment.cashnote.common.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableJpaAuditing
-@EnableScheduling
 class AppConfig
