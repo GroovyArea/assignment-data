bda665186cfd6121b04aacabd80e324b64197e29
GroovyArea <095201a@naver.com>
2024-07-10 16:08:29 +0900

    test : 데이터 전송 동의 목록 페이징 서비스 테스트

--- data-transfer/build.gradle.kts
+++ data-transfer/build.gradle.kts
@@ -11,18 +11,3 @@ apply(plugin = "com.epages.restdocs-api-spec")
 group = "com.groovyarea.assignment-data"
 version = ConstantDataTransfer.VERSION
 java.sourceCompatibility = JavaVersion.toVersion(Dependency.targetJvmVersion)
-
-dependencies {
-    // test
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(module = "mockito-core")
-    }
-    testImplementation("com.ninja-squad:springmockk:${Dependency.springMockkVersion}")
-
-    // restdocs
-    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
-    testImplementation("com.epages:restdocs-api-spec-mockmvc:${Dependency.restdocsapiSpecVersion}")
-    testImplementation("com.epages:restdocs-api-spec-model:${Dependency.restdocsapiSpecVersion}")
-
-}
--- data-transfer/src/main/kotlin/com/groovyarea/assignment/datatransfer/domain/service/DataTransferAgreementQueryServiceTest.kt
+++
@@ -1,8 +0,0 @@
-package com.groovyarea.assignment.datatransfer.domain.service
-
-import com.groovyarea.assignment.datatransfer.infrastructure.db.jpa.DataTransferAgreementRepository
-
-private val repository = mockk<DataTransferAgreementRepository>()
-
-@InjectMockks
-class DataTransferAgreementQueryServiceTest
