51583529d587d8c50748b7be2b327d3d3ff18709
김남영 <groovyarea@gimnam-yeongs-MacBook-Pro.local>
2024-07-15 22:05:19 +0900

    refactor : 코드 리팩토링

--- cash-note/src/main/kotlin/com/groovyarea/assignment/cashnote/infrastructure/http/community/config/CommunityHttpConfig.kt
+++ cash-note/src/main/kotlin/com/groovyarea/assignment/cashnote/infrastructure/http/community/config/CommunityHttpConfig.kt
@@ -8,15 +8,11 @@ import com.groovyarea.assignment.cashnote.infrastructure.http.community.exceptio
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatusCode
-import org.springframework.http.client.ClientHttpResponse
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
-import java.net.URI
 
 @Configuration
 class CommunityHttpConfig {
@@ -40,12 +36,6 @@ class CommunityHttpConfig {
         return RestClient.builder()
             .baseUrl(communityUrl)
             .messageConverters { it.add(converter) }
-            .requestInterceptor { request, body, execution ->
-                logRequest(request.uri, request.method, request.headers, body)
-                val response = execution.execute(request, body)
-                logResponse(response)
-                response
-            }
             .defaultStatusHandler(
                 HttpStatusCode::is4xxClientError
             ) { _, response ->
@@ -61,28 +51,5 @@ class CommunityHttpConfig {
                 throw CommunityClientException()
             }
             .build()
-    }
-
-    private fun logRequest(
-        uri: URI,
-        method: HttpMethod,
-        headers: HttpHeaders,
-        body: ByteArray?,
-    ) {
-        logger.info("===== Request =====")
-        logger.info("URI: {}", uri)
-        logger.info("Method: {}", method)
-        logger.info("Headers: {}", headers)
-        logger.info("Body: {}", body?.let { String(it, Charsets.UTF_8) })
-    }
-
-    private fun logResponse(
-        response: ClientHttpResponse,
-    ) {
-        logger.info("===== Response =====")
-        logger.info("Status code: {}", response.statusCode)
-        logger.info("Headers: {}", response.headers)
-        val responseBody = response.body.readAllBytes().toString(Charsets.UTF_8)
-        logger.info("Body: {}", responseBody)
     }
 }
--- data-transfer/src/main/kotlin/com/groovyarea/assignment/datatransfer/infrastructure/db/jpa/CardTransactionRepository.kt
+++ data-transfer/src/main/kotlin/com/groovyarea/assignment/datatransfer/infrastructure/db/jpa/CardTransactionRepository.kt
@@ -3,10 +3,20 @@ package com.groovyarea.assignment.datatransfer.infrastructure.db.jpa
 import com.groovyarea.assignment.datatransfer.common.entity.ID
 import com.groovyarea.assignment.datatransfer.domain.entity.table.CardTransaction
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.time.LocalDateTime
 
 interface CardTransactionRepository : JpaRepository<CardTransaction, ID> {
 
+    @Query(
+        """
+            select ct
+            from CardTransaction ct
+            where ct.registrationNumber = :registrationNumber
+            and ct.dataTransferred = false
+            and DATE(ct.createdAt) >= DATE(:createdAt)
+        """
+    )
     fun findAllByRegistrationNumberAndDataTransferredIsFalseAndCreatedAtGreaterThanEqual(
         registrationNumber: String,
         createdAt: LocalDateTime,
